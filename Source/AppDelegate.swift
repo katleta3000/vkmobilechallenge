1cbc8a121a048d18c8bd6fdc316504f718e74b2d
Evgenii Rtishchev <e.rtishchev@gmail.com>
2018-11-09 22:09:54 +0300

    Organized project structure

---
+++ Source/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  VKChallenge
+//
+//  Created by Evgenii Rtishchev on 09/11/2018.
+//  Copyright © 2018 Evgenii Rtishchev. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+	var window: UIWindow?
+}
+
---
+++ Source/ViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ViewController.swift
+//  VKChallenge
+//
+//  Created by Evgenii Rtishchev on 09/11/2018.
+//  Copyright © 2018 Evgenii Rtishchev. All rights reserved.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {}
+
